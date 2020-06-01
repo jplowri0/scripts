f295999670ee88e1c13aa11601cee07e25b2ba47
root <root@localhost.localdomain>
2020-06-01 06:40:05 -0400

    Commenting weeks 1

--- portfolio/week1/second.sh
+++ portfolio/week1/second.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-echo "Top of the morning to you"
-echo "Grand day it is $1!"
-exit 0
+echo "Top of the morning to you" #Here the code is greeting the user. 
+echo "Grand day it is $1!" #When the user executes the code with ./second.sh they must also apply an argument to be set to the $1 variable. 
+exit 0 #The code exits without error. 
+
+#This code was referred to by CYB6004 Weekly Exercise 1.4 Step 4. 
