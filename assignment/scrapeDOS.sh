221661836286ba0dc9e6f672dbee525322aeacee
jplow <jplow21@gmail.com>
2020-06-08 10:22:19 +1000

    scraping code initial

---
+++ assignment/scrapeDOS.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#REF E
+wget "https://www.cvedetails.com/vulnerability-list.php?vendor_id=49&product_id=32536&version_id=&page={1..6}&hasexp=0&opdos=1&opec=0&opov=0&opcsrf=0&opgpriv=0&opsqli=0&opxss=0&opdirt=0&opmemc=0&ophttprs=0&opbyp=0&opfileinc=0&opginf=0&cvssscoremin=0&cvssscoremax=0&year=0&month=0&cweid=0&order=1&trc=268&sha=1d1222966d7d806946195eced2cf2173dc2e3baa";
+
