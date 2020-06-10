bb95520828739004774f138ecafdb868cd77e45f
root <root@localhost.localdomain>
2020-06-09 22:40:26 -0400

    CSV component built

--- assignment/workingData/DOSdesCode/DOSdesCodeConcatenator.py
+++ assignment/workingData/DOSdesCode/DOSdesCodeConcatenator.py
@@ -1,12 +1,13 @@
 #This script will collect all of the text files from the sed loop and 
 #concatenate them all together as one complete text file. 
-#REF https://stackoverflow.com/questions/17749058/combine-multiple-text-files-into-one-text-file-using-python
+#REF https://stackoverflow.com/questions/13613336/python-concatenate-text-files
 
-import glob
 
-read_files = glob.glob("*.txt")
 
-with open("DOSdesCodeAll.txt", "wb") as outputFiles:
-    for f in read_files:
-        with open(f, "rb") as inputFiles:
-            outputFiles.write(inputFiles.read())
+
+filenames = ['DesCode1.txt','DesCode2.txt','DesCode3.txt','DesCode4.txt','DesCode5.txt','DesCode6.txt']
+with open('DOSdesCodeAll.txt', 'w') as outputFiles:
+    for i in filenames:
+        with open(i) as inputFiles:
+            for line in inputFiles:
+                outputFiles.write(line)
--- assignment/workingData/DOSratingCode/DOSratingCodeConcatenator.py
+++ assignment/workingData/DOSratingCode/DOSratingCodeConcatenator.py
@@ -1,12 +1,13 @@
 #This script will collect all of the text files from the sed loop and 
 #concatenate them all together as one complete text file. 
-#REF https://stackoverflow.com/questions/17749058/combine-multiple-text-files-into-one-text-file-using-python
+#REF https://stackoverflow.com/questions/13613336/python-concatenate-text-files
 
-import glob
 
-read_files = glob.glob("*.txt")
 
-with open("DOSratingCodeAll.txt", "wb") as outputFiles:
-    for f in read_files:
-        with open(f, "rb") as inputFiles:
-            outputFiles.write(inputFiles.read())
+
+filenames = ['RatingCode1.txt','RatingCode2.txt','RatingCode3.txt','RatingCode4.txt','RatingCode5.txt','RatingCode6.txt']
+with open('DOSratingCodeAll.txt', 'w') as outputFiles:
+    for i in filenames:
+        with open(i) as inputFiles:
+            for line in inputFiles:
+                outputFiles.write(line)
