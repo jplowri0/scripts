7a7b9107e4c524ee37f5322707e9917507304769
root <root@localhost.localdomain>
2020-06-14 06:05:30 -0400

    GainPriveleges Component Complete

---
+++ assignment/scrapePriveledges.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#REF E
+
+#Below uses a for loop to scrape mulitple of pages. This is due to that there are only 50 records per page. So we must cycle through each 
+#page in order to get all the data. I set a range for page 1 to page 6 as there are only 6 pages. 
+#The files are renamed to watchOSdos.html for clarity and are saved in the scrapedData directory. 
+
+for ((i=1;i<2;++i)); do 
+    wget "https://www.cvedetails.com/vulnerability-list.php?vendor_id=49&product_id=32536&version_id=&page=$i&hasexp=0&opdos=0&opec=0&opov=0&opcsrf=0&opgpriv=1&opsqli=0&opxss=0&opdirt=0&opmemc=0&ophttprs=0&opbyp=0&opfileinc=0&opginf=0&cvssscoremin=0&cvssscoremax=0&year=0&month=0&cweid=0&order=1&trc=25&sha=e3edbf3e6424900c3fae69a3071a0191e3c475e8" -O scrapedData/Priveleges/watchOSpriveleges$i.html
+done
